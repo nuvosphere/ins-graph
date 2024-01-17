7e2a1b721324e634b9ada5dea7681764d83d14fd
EC2 Default User <bossyuansu@gmail.com>
2024-01-17 08:00:46 +0000

    he

---
+++ subgraph/src/Nip20Market.ts
@@ -0,0 +1,40 @@
+import {
+  OrderCreated,
+  OrderRemoved,
+  OrderExecuted
+} from "../generated/Nip20Market/Nip20Market";
+import { MarketOrder } from "../generated/schema";
+
+export function handleNewOrder(event: OrderCreated): void {
+  let order = new MarketOrder(event.params.orderId.toHex());
+  //uint256 indexed orderId, address indexed seller, uint256 price, bytes32 ticker, bytes32 txhash
+  order.owner = event.params.seller;
+  order.price = event.params.price;
+  order.ticker = event.params.ticker;
+  order.inscription = event.params.txhash;
+  order.active = "true";
+  order.buyer = event.params.seller;
+  order.save();
+}
+
+export function handleRemoveOrder(event: OrderRemoved): void {
+  let order = new MarketOrder(event.params.orderId.toHex());
+  //uint256 indexed orderId, address indexed seller, uint256 price, bytes32 ticker, bytes32 txhash
+  if (!order) {
+    order = new MarketOrder(event.params.orderId.toHex());
+  }
+  order.active = "false";
+  order.save();
+}
+
+
+export function handleExecuteOrder(event: OrderExecuted): void {
+  let order = new MarketOrder(event.params.orderId.toHex());
+  //uint256 indexed orderId, address indexed seller, uint256 price, bytes32 ticker, bytes32 txhash
+  if (!order) {
+    order = new MarketOrder(event.params.orderId.toHex());
+  }
+  order.buyer = event.params.buyer;
+  order.active = "false";
+  order.save();
+}
